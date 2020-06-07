4bf455c70fb1fa5b937fde537da3e9ea6ff7e53f
jtwalker126 <j.walker@vanderbilt.edu>
2020-06-07 11:12:16 -0500

    adding challenge code

--- apps/app.py
+++ apps/app.py
@@ -11,14 +11,23 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+   hemisphere = mongo.db.hemisphere
+   hemispheres = [elm for elm in hemisphere.find({})]
+   print(hemispheres)
+   return render_template("index.html", mars=mars, hemispheres=hemispheres)
 
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
+   hemisphere = mongo.db.hemisphere
+   hemisphere.delete_many({})
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+   hemisphere_data = scraping.hemisphere_images()
+   #print(hemisphere_data)
+   hemisphere.insert_many(hemisphere_data)
+   return "Scraping successful!"
 
 if __name__ == "__main__":
     app.run()
+   
